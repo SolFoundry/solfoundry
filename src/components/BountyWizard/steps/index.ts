01540825ee3d661b610024fde3f642d64b560b7f
LaphoqueRC <91871936+LaphoqueRC@users.noreply.github.com>
2026-03-19 16:44:38 +0300

    feat: add multi-step bounty creation wizard

---
+++ src/components/BountyWizard/steps/index.ts
@@ -0,0 +1,7 @@
+export { default as TierStep } from './TierStep';
+export { default as DetailsStep } from './DetailsStep';
+export { default as RequirementsStep } from './RequirementsStep';
+export { default as CategoryStep } from './CategoryStep';
+export { default as RewardStep } from './RewardStep';
+export { default as PreviewStep } from './PreviewStep';
+export { default as ConfirmStep } from './ConfirmStep';
