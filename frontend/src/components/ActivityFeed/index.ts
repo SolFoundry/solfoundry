45a71cbcc17fa576b6ed4c9b1f7e70abf30663d6
LaphoqueRC <91871936+LaphoqueRC@users.noreply.github.com>
2026-03-21 02:19:34 +0300

    feat(frontend): index

---
+++ frontend/src/components/ActivityFeed/index.ts
@@ -0,0 +1,9 @@
+export { default as ActivityFeed } from './ActivityFeed';
+export { ActivityEventCard } from './ActivityEventCard';
+export { EventIcon } from './EventIcon';
+export type {
+  ActivityEvent,
+  EventType,
+  ActivityFeedProps,
+  EventIconProps
+} from './types';
