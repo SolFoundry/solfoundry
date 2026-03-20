0ea006de4188252b4087abd02ea44f5727b0cc83
zengbao yu <43713212+yuzengbaao@users.noreply.github.com>
2026-03-21 02:46:04 +0800

    feat: add frontend/src/__tests__/HealthPage.test.tsx

---
+++ frontend/src/__tests__/HealthPage.test.tsx
@@ -0,0 +1,8 @@
+"""Tests for HealthPage."""
+import { HealthPage } from "../pages/HealthPage";
+
+describe("HealthPage", () => {
+    it("should render", () => {
+        expect(HealthPage).toBeDefined();
+    });
+});
