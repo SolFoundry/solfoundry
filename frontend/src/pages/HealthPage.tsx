ebc390c31e3e23d25d4451353c1c587900b8732b
zengbao yu <43713212+yuzengbaao@users.noreply.github.com>
2026-03-21 02:46:01 +0800

    feat: add frontend/src/pages/HealthPage.tsx

---
+++ frontend/src/pages/HealthPage.tsx
@@ -0,0 +1,8 @@
+"""Health and Status page component."""
+
+import React from "react";
+
+/** View component for platform health check */
+export function HealthPage() {
+    return <div>Platform is Healthy</div>;
+}
