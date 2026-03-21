2ef4d86388aeb54eb0650d5e1293e6f22ef90b36
James Harrison .S <40062598+KodeSage@users.noreply.github.com>
2026-03-21 10:43:05 +0100

    chores: toast systetm (#327)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,6 +21,10 @@ export default {
           '0%': { opacity: '0', transform: 'translateY(-8px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         },
+        'toast-progress': {
+          '0%': { width: '100%' },
+          '100%': { width: '0%' },
+        },
       },
     } 
   } 
